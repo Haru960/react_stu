362f9950f4386f6063e2f497819640fffa298895
Haru960 <jjinson456@gamil.com>
2021-03-22 16:34:05 +0900

    003 Add

---
+++ client/003/src/R001_ImportConponent.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+class R001_ImportConponent extends Component{
+	render(){
+		return (
+			<h2>[ THIS IS IMPORTED COMPONENT ]</h2>
+		)
+	}
+}
+
+export default ROO1_ImportConponent;
