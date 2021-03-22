b714ccff64d33e1d455a62b6fa9d1d96e6d8f5de
Haru960 <jjinson456@gamil.com>
2021-03-22 16:54:19 +0900

    004 ADD

---
+++ client/004/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import LifecycleEx from './R007_LifecycleEx'
+
+function App() {
+  return (
+    <div>
+      <h1>Start React 200!</h1>
+      <p>CSS 적용하기</p>
+      <LifecycleEx prop_value = 'FromApp.js'></LifecycleEx>
+    </div>
+  );
+}
+
+export default App;
